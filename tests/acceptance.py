9db0be0ba4f91507a3524d742b390cc0cc1f0ae6
Ben Lambert <ben.lambert@cloudacademy.com>
2016-06-06 15:00:51 -0400

    changes test

--- tests/acceptance.py
+++ tests/acceptance.py
@@ -2,19 +2,14 @@ from splinter import Browser
 import sys
 
 
-with Browser('phantomjs', wait_time=10) as browser:
+with Browser('phantomjs') as browser:
     # Visit URL
     url = sys.argv[1]
     browser.visit(url)
-    print(url)
 
-    headers = browser.find_by_tag('header').first
 
-    h1 = headers.find_by_tag('h1').first
 
-    print(h1)
-
-    if h1.value == "todo":
-        sys.exit(1)
-    else:
+    if browser.is_text_present('todo', wait_time=10):
         sys.exit(0)
+    else:
+        sys.exit(1)
