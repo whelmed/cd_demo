bb1b32823341a8db87b454881bfdb78ddf1cbe19
Ben Lambert <ben.lambert@cloudacademy.com>
2016-06-06 15:11:45 -0400

    changes test

--- tests/acceptance.py
+++ tests/acceptance.py
@@ -7,9 +7,11 @@ with Browser('phantomjs') as browser:
     url = sys.argv[1]
     browser.visit(url)
 
-    print("starting test on " + url)
+    print("Starting test on " + url)
 
     if browser.is_text_present('todo', wait_time=10):
+        print("Success")
         sys.exit(0)
     else:
+        print("Failed")
         sys.exit(1)
