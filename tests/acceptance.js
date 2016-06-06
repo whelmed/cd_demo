a554a5ae99e9f8e5de45dc0dc2ea679098429a6b
Ben Lambert <ben.lambert@cloudacademy.com>
2016-06-06 12:57:45 -0400

    adds tests

---
+++ tests/acceptance.js
@@ -0,0 +1,36 @@
+var page = require('webpage').create(),
+  system = require('system'),
+  t, address;
+
+console.error = function () {
+    require("system").stderr.write(Array.prototype.join.call(arguments, ' ') + '\n');
+};
+
+
+if (system.args.length === 1) {
+  console.log('Usage: acceptance.js <some URL>');
+  phantom.exit();
+}
+
+t = Date.now();
+address = system.args[1];
+
+// This isn't a "REAL" test. It's just a basic get for our demo.
+page.open(address, function(status) {
+  if (status !== 'success') {
+    console.error('FAILED to load the address');
+  } else {
+    t = Date.now() - t;
+    console.log('Loading ' + system.args[1]);
+    var content = document.querySelector("header h1").textContent;
+
+    if (content === "todo") {
+      console.log('CONTENT todo exists!');
+    }
+    else {
+      console.error("CONTENT todo is missing! This is essential for the application...probably!")
+    }
+
+  }
+  phantom.exit();
+});
