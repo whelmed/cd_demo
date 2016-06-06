0912bb2af209083e38638d073f74a7dc2ae4b885
Ben Lambert <ben.lambert@cloudacademy.com>
2016-06-06 14:06:26 -0400

    adjusts test runner

--- tests/acceptance.js
+++ tests/acceptance.js
@@ -15,10 +15,27 @@ if (system.args.length === 1) {
 address = system.args[1];
 
 
-page.onConsoleMessage = function(msg) {
-  if (msg == "ERROR") {
-    phantom.exit(1);
-  }
+function waitFor(testFx, onReady, timeOutMillis) {
+    var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 3000, //< Default Max Timout is 3s
+        start = new Date().getTime(),
+        condition = false,
+        interval = setInterval(function() {
+            if ( (new Date().getTime() - start < maxtimeOutMillis) && !condition ) {
+                // If not time-out yet and condition not yet fulfilled
+                condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
+            } else {
+                if(!condition) {
+                    // If condition still not fulfilled (timeout but condition is 'false')
+                    console.log("'waitFor()' timeout");
+                    phantom.exit(1);
+                } else {
+                    // Condition fulfilled (timeout and/or condition is 'true')
+                    console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
+                    typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
+                    clearInterval(interval); //< Stop this interval
+                }
+            }
+        }, 250); //< repeat check every 250ms
 };
 
 // This isn't a "REAL" test. It's just a basic get for our demo.
@@ -28,18 +45,24 @@ page.open(address, function(status) {
   } else {
 
     console.log('Loading ' + system.args[1]);
-
-    page.evaluate(function() {
-      var content = document.querySelector("header h1").textContent;
-      if (content === "todont") {
-        console.log('CONTENT todo exists!');
-      }
-      else {
-        console.log("ERROR");
-        console.error("ERROR! CONTENT todo is missing! This is essential for the application...probably!");
-      }
-
+    waitFor(function() {
+        // Check in the page if a specific element is now visible
+        return page.evaluate(function() {
+          var content = document.querySelector("header h1").textContent;
+          if (content === "todont") {
+            console.log('CONTENT todo exists!');
+            return true;
+          }
+          else {
+            console.error("ERROR! CONTENT todo is missing! This is essential for the application...probably!");
+            return false;
+          }
+        });
+    }, function() {
+       console.log("The sign-in dialog should be visible now.");
+       phantom.exit();
     });
+
 
   }
   phantom.exit();
